179a8d02f41291093b279253ae086f947983757b
NGalygich <NGalygich@yandex.ru>
2022-05-21 21:39:34 +0300

    Dowmland file

--- Program.cs
+++ Program.cs
@@ -1,79 +1,78 @@
-﻿using Telegram.Bot;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
 using Telegram.Bot.Extensions.Polling;
 using Telegram.Bot.Types;
+using Telegram.Bot.Exceptions;
 
-
-namespace Project_9
+namespace Project_9;
+class Program
 {
-
-    class Program
+    static ITelegramBotClient bot = new TelegramBotClient("5313407205:AAHpDxT2Pjo_0VTSJRtqLuDutSvi1bea7Bk");
+    public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
-        //public string path1 = @"C:\Users\nikit\Desktop\SkillBox";
-        static ITelegramBotClient bot = new TelegramBotClient("TOKEN");
-        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        // Некоторые действия
+        Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+        if(update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
         {
-            // Некоторые действия
-            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
-            if(update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            var message = update.Message;
+            /*
+            if (message.Text.ToLower() == "/start")
             {
-                var message = update.Message;
-                if (message.Text.ToLower() == "/start")
-                {
-                    await botClient.SendTextMessageAsync(message.Chat, "Выберите команду.");
-                    await botClient.SendTextMessageAsync(message.Chat, "/view - Просмотр сохраненных файлов;");
-                    await botClient.SendTextMessageAsync(message.Chat, "/save - Сохранить файл;");
-                    await botClient.SendTextMessageAsync(message.Chat, "/download - Скачать файл;");
-                    return;
-                }
-                if (message.Text.ToLower() == "/view")
-                {
-                    await botClient.SendTextMessageAsync(message.Chat, "Просмотр сохраненных файлов;");
-                    return;
-                }
-                if (message.Text.ToLower() == "/save")
-                {
-                    await botClient.SendTextMessageAsync(message.Chat, "Сохранить файл;");
-                    DownLoad(message.Document.FileId, message.Document.FileName);
-
-                    return;
-                }
-                 if (message.Text.ToLower() == "/download")
-                {
-                    await botClient.SendTextMessageAsync(message.Chat, "Скачать файл;");
-                    return;
-                }
-                await botClient.SendTextMessageAsync(message.Chat, "Привет, выбери команду. Чтобы просмотреть команды введи /start");
+                await botClient.SendTextMessageAsync(message.Chat, "Выберите действие:");
+                await botClient.SendTextMessageAsync(message.Chat, "Загрузить файл на сревер /input");
+                await botClient.SendTextMessageAsync(message.Chat, "Выгрузить файл с сервера /output");
+                await botClient.SendTextMessageAsync(message.Chat, "Просмотреть файлы на сервере /view");
+                return;
             }
+            */
+            if (message.Document != null)
+            {
+                Console.WriteLine($"<<File ID>> = {message.Document.FileId}");
+                var file = await bot.GetFileAsync(message.Document.FileId);
+                FileStream fs = new FileStream("_" + message.Document.FileName, FileMode.Create);
+                await bot.DownloadFileAsync(file.FilePath, fs);
+                fs.Close();
+                fs.Dispose();
+                await botClient.SendTextMessageAsync(message.Chat, "Файл загружен");
+                return;
+            }
+            else if (message.Audio != null)
+            {
+                Console.WriteLine($"<<File ID>> = {message.Audio.FileId}");
+                var file = await bot.GetFileAsync(message.Audio.FileId);
+                FileStream fs = new FileStream("_" + message.Audio.FileName, FileMode.Create);
+                await bot.DownloadFileAsync(file.FilePath, fs);
+                fs.Close();
+                fs.Dispose();
+                await botClient.SendTextMessageAsync(message.Chat, "Аудио файл загружен");
+                return;
+            }
+            await botClient.SendTextMessageAsync(message.Chat, "Я не умею общаться. Только скачивать и выгружать файлы. /start");
         }
+    }
 
-        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        // Некоторые действия
+        Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+    }
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
+
+        var cts = new CancellationTokenSource();
+        var cancellationToken = cts.Token;
+        var receiverOptions = new ReceiverOptions
         {
-            // Некоторые действия
-            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
-        }
+            AllowedUpdates = { }, // receive all update types
+        };
 
-        static async void DownLoad(string fileId, string path){
-            var file = await bot.GetFileAsync(fileId);
-            FileStream fs = new FileStream("_" + path, FileMode.Create);
-            await bot.DownloadFileAsync(file.FilePath, fs);
-            fs.Close();
-            fs.Dispose();
-        }
+        bot.StartReceiving(HandleUpdateAsync,HandleErrorAsync,receiverOptions,cancellationToken);
 
-        static void Main(string[] args)
-        {
-            
-            Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
-
-            var cts = new CancellationTokenSource();
-            var cancellationToken = cts.Token;
-            var receiverOptions = new ReceiverOptions {
-                AllowedUpdates = { }, // receive all update types
-            };
-            bot.StartReceiving(HandleUpdateAsync, HandleErrorAsync, receiverOptions, cancellationToken);
-            Console.ReadLine();
-        }
-
-        
+        Console.ReadLine();
     }
 }
